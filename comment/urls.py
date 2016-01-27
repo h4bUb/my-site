ad18fe4e9fd316cc64a9091bd41a4df7adf125d2
h4bUb <habub.games@gmail.com>
2016-01-27 21:40:19 +0500

    Stable version

--- comment/urls.py
+++ comment/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^#about$', views.post_list_about, name='post_list_about'),
+    url(r'^#contact$', views.post_list_contact, name='post_list_contact'),
+    url(r'^gallery/$', views.gallery, name='gallery'),
 ]
--- comment/views.py
+++ comment/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+#from .models import Post
 
 def post_list(request):
     return render(request, 'comment/index.html', {})
+
+def post_list_about(request):
+    return render(request, 'comment/index.html#about', {})
+
+def post_list_contact(request):
+    return render(request, 'comment/index.html#contact', {})
+
+def gallery(request):
+    return render(request, 'comment/gallery.html', {})
