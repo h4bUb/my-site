4819df3b0add7d3c797cbb53373154c555a1282e
h4bUb <habub.games@gmail.com>
2016-01-27 22:45:57 +0500

    Stable version

--- comment/urls.py
+++ comment/urls.py
@@ -4,7 +4,6 @@ import urllib
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^#about$', views.post_list_about, name='post_list_about'),
-    url(r'^#contact$', views.post_list_contact, name='post_list_contact'),
+    url(r'^index/$', views.post_list, name='post_list'),
     url(r'^gallery/$', views.gallery, name='gallery'),
 ]
--- comment/views.py
+++ comment/views.py
@@ -6,11 +6,5 @@ from django.utils.html import escape
 def post_list(request):
     return render(request, 'comment/index.html', {})
 
-def post_list_about(request):
-    return render(request, escape(iri_to_uri(u"comment/index.html#about")), {})
-
-def post_list_contact(request):
-    return render(request, escape(iri_to_uri(u"comment/index.html#contact")), {})
-
 def gallery(request):
     return render(request, 'comment/gallery.html', {})
