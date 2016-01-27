5aae175db85ee2630b19b8cd714a2d3f76f610c5
h4bUb <habub.games@gmail.com>
2016-01-27 22:36:17 +0500

    Stable version

--- comment/views.py
+++ comment/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-import urllib
+#from django.utils.http import urlencode
+from django.utils.encoding import iri_to_uri
+from django.utils.html import escape
 
 def post_list(request):
     return render(request, 'comment/index.html', {})
 
 def post_list_about(request):
-    return render(request, urllib.urlencode(r'comment/index.html#about'), {})
+    return render(request, escape(iri_to_uri(u"comment/index.html#about")), {})
 
 def post_list_contact(request):
-    return render(request, urllib.urlencode(r'comment/index.html#contact'), {})
+    return render(request, escape(iri_to_uri(u"comment/index.html#contact")), {})
 
 def gallery(request):
     return render(request, 'comment/gallery.html', {})
